98ca9362c34e14eeacef7aab8addfcf9c56ce009
guoguo <guoguo@mini-m1.local>
2023-04-29 23:26:16 +0800

    Day 5 - Solution 1

---
+++ solutions/day5.ts
@@ -0,0 +1,41 @@
+import readByLine from '../utils/readTxtByLine';
+
+const solutions = async () => {
+    const lines = readByLine(5);
+    const arr: Array<string> = new Array(9).fill('');
+    for await (const line of lines) {
+        // original stack
+        if (line.trim().startsWith('[')) {
+            const len = line.length;
+            for (let i = 0; i < len; i += 4) {
+                const word = line.substring(i, i + 4).trim();
+                if (word) {
+                    arr[i / 4] += word[1];
+                }
+            }
+        }
+
+        if (line.startsWith('move')) {
+            let [, count, , from, , to] = line
+                .split(' ')
+                .filter((item) => item)
+                .map((item) => Number(item));
+            from -= 1;
+            to -= 1;
+            for (let i = 0; i < count; i++) {
+                if (arr[from]) {
+                    arr[to] = arr[from][0] + arr[to];
+                    arr[from] = arr[from].substring(1);
+                }
+            }
+        }
+    }
+    const result = arr
+        .filter((item) => item)
+        .map((item) => item[0])
+        .join('');
+    console.log(result);
+    return result;
+};
+
+export default solutions;
--- solutions/index.ts
+++ solutions/index.ts
@@ -1,4 +1,4 @@
-import solutions from './day4';
+import solutions from './day5';
 
 const res = solutions();
 console.log(res);
