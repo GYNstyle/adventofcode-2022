47e9614f99580977a2e86d46a07b125afb5ed973
guoguo <guoguo@Yifus-MacBook-Air.local>
2023-05-17 21:57:23 +0800

    Refactor for day 7

--- solutions/day7.js
+++ solutions/day7.js
@@ -1,106 +1,114 @@
-const fs = require("node:fs");
+import { readFileSync } from "node:fs";
+
+const dirType = 'dir';
+const fileType = 'file';
 
 function TreeNode(name, type, parent, size) {
-  this.name = name || null;
-  this.type = type || "dir";
-  this.parent = parent || null;
-  this.size = size || 0;
-  this.children = {};
+    this.name = name || null;
+    this.type = type || dirType;
+    this.parent = parent || null;
+    this.size = size || 0;
+    this.children = {};
 }
 
-const root = new TreeNode("root", "dir", null);
-let currentDir = root;
-
-const commandLine = (input) => {
-  const [_, command, dir] = input.split(" ");
-  if (command === "ls") {
-    return;
-  }
-  if (command === "cd") {
-    if (dir === "..") {
-      // go back to parent directory
-      currentDir = currentDir.parent;
-    } else if (dir === "/") {
-      currentDir = root;
-    } else {
-      currentDir = currentDir.children[dir];
+const commandLine = (input, root, currentDir) => {
+    const [_, command, dir] = input.split(' ');
+    if (command === 'ls') {
+        return currentDir;
     }
-  }
-  return;
+    if (command === 'cd') {
+        if (dir === '..') {
+            return currentDir.parent;
+        }
+        if (dir === '/') {
+            return root;
+        }
+        return currentDir.children[dir];
+    }
 };
 
 const contentLine = (line) => {
-  const [dirOrSize, name] = line.split(" ");
-  if (dirOrSize === "dir") {
-    currentDir.children[name] = new TreeNode(name, "dir", currentDir);
-  } else {
-    currentDir.children[name] = new TreeNode(
-      name,
-      "file",
-      currentDir,
-      Number(dirOrSize)
-    );
-  }
+    const [dirOrSize, name] = line.split(' ');
+    if (dirOrSize === dirType) {
+        return {
+            name,
+            type: dirType,
+            size: 0,
+        };
+    }
+    return {
+        name,
+        type: fileType,
+        size: Number(dirOrSize),
+    };
 };
 
-const fileContent = fs.readFileSync("day7.txt", "utf-8");
-fileContent.split(/r?\n/).forEach((line) => {
-  if (line.startsWith("$")) {
-    commandLine(line);
-  } else {
-    contentLine(line);
-  }
-});
-
 const sumNodeSize = (node) => {
-  if (node.type === "dir") {
-    Object.keys(node.children).map((item) => {
-      const nextNode = node.children[item];
-      node.size += sumNodeSize(nextNode);
-    });
-  }
-  return node.size;
+    if (node.type === dirType) {
+        Object.keys(node.children).map((item) => {
+            const nextNode = node.children[item];
+            node.size += sumNodeSize(nextNode);
+        });
+    }
+    return node.size;
 };
 
 const getTotalSizeUnderA = (node) => {
-  let res = 0;
-  if (node.type === "dir" && node.size < 100000) {
-    res += node.size;
-  }
-  Object.keys(node.children).forEach((item) => {
-    const nextNode = node.children[item];
-    res += getTotalSizeUnderA(nextNode);
-  });
-  return res;
-};
-
-sumNodeSize(root);
-const result = getTotalSizeUnderA(root);
-
-const minSize = 30000000 - (70000000 - root.size);
-const getSmallestDirToDelete = () => {
-  let resultTwo = root.size;
-  if (root.size < minSize) {
-    return false;
-  }
-  const dfsFunc = (node) => {
-    if (node.type === "dir" && node.size >= minSize) {
-      resultTwo = Math.min(resultTwo, node.size);
-      Object.keys(node.children).forEach((item) => {
-        const nextNode = node.children[item];
-        dfsFunc(nextNode, resultTwo);
-      });
+    let res = 0;
+    if (node.type === dirType && node.size < 100000) {
+        res += node.size;
     }
-  };
-  dfsFunc(root);
-  return resultTwo;
+    Object.keys(node.children).forEach((item) => {
+        const nextNode = node.children[item];
+        res += getTotalSizeUnderA(nextNode);
+    });
+    return res;
 };
 
-const resultTwo = getSmallestDirToDelete();
+const getSmallestDirToDelete = (root, minSize) => {
+    let resultTwo = root.size;
+    if (root.size < minSize) {
+        return false;
+    }
+    const dfsFunc = (node) => {
+        if (node.type === dirType && node.size >= minSize) {
+            resultTwo = Math.min(resultTwo, node.size);
+            Object.keys(node.children).forEach((item) => {
+                const nextNode = node.children[item];
+                dfsFunc(nextNode, resultTwo);
+            });
+        }
+    };
+    dfsFunc(root);
+    return resultTwo;
+};
 
 const solution = () => {
-  return [result, resultTwo]
-}
+    const root = new TreeNode('root', dirType, null);
+    let currentDir = root;
 
-export default solution;
+    const fileContent = readFileSync('./inputs/7.txt', 'utf-8');
+    fileContent.split(/r?\n/).forEach((line) => {
+        if (line.startsWith('$')) {
+            currentDir = commandLine(line, root, currentDir);
+        } else {
+            const obj = contentLine(line, currentDir);
+            const { name, type, size } = obj;
+            currentDir.children[name] = new TreeNode(
+                name,
+                type,
+                currentDir,
+                size
+            );
+        }
+    });
+    sumNodeSize(root);
 
+    const solutionOne = getTotalSizeUnderA(root);
+    const minSize = 30000000 - (70000000 - root.size);
+    const solutionTwo = getSmallestDirToDelete(root, minSize);
+
+    return [solutionOne, solutionTwo];
+};
+
+solution();
